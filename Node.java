a7d2d375b08fe715b71c44717398e14e8975d45e
herni <43722963+Hernifatir@users.noreply.github.com>
2018-10-09 23:58:04 +0800

    Create Node.java

---
+++ Node.java
@@ -0,0 +1,21 @@
+
+public class node {
+	//Attributes
+	String Label;
+	int value ;
+	public String getLabel() {
+		return Label;
+	}
+	public void setLabel(String label) {
+		Label = label;
+	}
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
+	
+	
+
+}
