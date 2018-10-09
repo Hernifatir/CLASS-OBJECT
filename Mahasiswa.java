994c09b7823e5fcb45e83e751af3ab2645f44318
herni <43722963+Hernifatir@users.noreply.github.com>
2018-10-09 23:57:13 +0800

    Create Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,54 @@
+
+public class Mahasiswa {
+	//attributtes
+		String nama;
+		String nim;
+		String alamat;
+		String golonganDarah;
+		String status;
+		int tinggiBadan;
+		int beratBaran;
+		public String getNama() {
+			return nama;
+		}
+		public void setNama(String nama) {
+			this.nama = nama;
+		}
+		public String getNim() {
+			return nim;
+		}
+		public void setNim(String nim) {
+			this.nim = nim;
+		}
+		public String getAlamat() {
+			return alamat;
+		}
+		public void setAlamat(String alamat) {
+			this.alamat = alamat;
+		}
+		public String getGolonganDarah() {
+			return golonganDarah;
+		}
+		public void setGolonganDarah(String golonganDarah) {
+			this.golonganDarah = golonganDarah;
+		}
+		public String getStatus() {
+			return status;
+		}
+		public void setStatus(String status) {
+			this.status = status;
+		}
+		public int getTinggiBadan() {
+			return tinggiBadan;
+		}
+		public void setTinggiBadan(int tinggiBadan) {
+			this.tinggiBadan = tinggiBadan;
+		}
+		public int getBeratBaran() {
+			return beratBaran;
+		}
+		public void setBeratBaran(int beratBaran) {
+			this.beratBaran = beratBaran;
+		}
+		
+}
