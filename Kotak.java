a88d1de11b2eea14fc7c2bbb46919d9bc98fca9b
herni <43722963+Hernifatir@users.noreply.github.com>
2018-10-09 23:55:21 +0800

    Create Kotak.java

---
+++ Kotak.java
@@ -0,0 +1,90 @@
+
+public class kotak {
+	private double Panjang, Lebar, Luas, Keliling;
+	
+		// ini method getter
+		public double getPanjang()
+		{
+			return Panjang;
+		}
+		public double getLebar()
+		{
+			return Lebar;
+		}
+		public double getKeliling()
+		{
+			return Keliling;
+		}
+		public void setLebar(double lebar)
+		{
+			this.Lebar = Lebar;
+		}
+		public void setLuas(double Luas)
+		{
+			this.Luas = Luas;
+		}
+		public void setPanjang(double Panjang)
+		{
+			this.Panjang =Panjang;
+		}
+		public void setKeliling(double Keliling)
+		{
+			this.Keliling = Keliling;
+		}
+	//method
+		void hitungLuas(){
+		Luas = Panjang * Lebar;
+		}
+		void hitungaKeliling(){
+			Keliling= 2*Panjang + 2*Lebar;
+		}
+		
+}
+		
+
+
+
+public class kotak {
+	private double Panjang, Lebar, Luas, Keliling;
+	
+		// ini method getter
+		public double getPanjang()
+		{
+			return Panjang;
+		}
+		public double getLebar()
+		{
+			return Lebar;
+		}
+		public double getKeliling()
+		{
+			return Keliling;
+		}
+		public void setLebar(double lebar)
+		{
+			this.Lebar = Lebar;
+		}
+		public void setLuas(double Luas)
+		{
+			this.Luas = Luas;
+		}
+		public void setPanjang(double Panjang)
+		{
+			this.Panjang =Panjang;
+		}
+		public void setKeliling(double Keliling)
+		{
+			this.Keliling = Keliling;
+		}
+	//method
+		void hitungLuas(){
+		Luas = Panjang * Lebar;
+		}
+		void hitungaKeliling(){
+			Keliling= 2*Panjang + 2*Lebar;
+		}
+		
+}
+		
+
+
